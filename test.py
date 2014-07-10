b519e4a558ae559b3c27cc8e646dc2285f5a88b7
superbirds <superbirds2008@gmailcom>
2014-07-10 09:46:54 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+for i in range(5,11):
+    print i
+else:
+    print 'done'
+
+
+while True:
+    s=raw_input('Enter something:')
+
+    if s=='quit':
+        break
+    b='Length of the string is',len(s)
+    print b
+
+print 'finished'
